201814fdfe2f9edab9e25b3770b34c174bc20f9c
Tayfun Sur <tayfunsur@gmail.com>
2020-04-29 13:54:56 +0300

    author create function and template are added

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,9 @@
 let Author = require('../models/author');
 let Book = require('../models/book');
 
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 const async = require('async');
 
 //author list
@@ -27,8 +30,8 @@ exports.author_detail = function (req, res, next) {
         Author.findById(req.params.id).exec(callback);
       },
       books: (callback) => {
-        Book.find({ author: req.params.id }, 'title summary').exec(callback)
-      } 
+        Book.find({ author: req.params.id }, 'title summary').exec(callback);
+      },
     },
     (err, results) => {
       if (err) {
@@ -39,20 +42,71 @@ exports.author_detail = function (req, res, next) {
         err.status = 404;
         return next(err);
       } else {
-        res.render('author_detail', {title: 'author detail', author:results.author, books:results.books})
+        res.render('author_detail', {
+          title: 'author detail',
+          author: results.author,
+          books: results.books,
+        });
       }
-      console.log(results.books)
+      console.log(results.books);
     }
   );
 };
 
 exports.author_create_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: author create get');
+  res.render('author_form', { title: 'author create' });
 };
 
-exports.author_create_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: author create post');
-};
+exports.author_create_post = [
+  //validate fields
+  body('first_name')
+    .trim()
+    .isLength({ min: 2 }).withMessage('first name must be specified')
+    .isAlphanumeric()
+    .withMessage('first name has non alphanumerical chars'),
+  body('family_name')
+    .trim()
+    .isLength({ min: 2 }).withMessage('family name must be specified')
+    .isAlphanumeric()
+    .withMessage('family name has non alphanumerical chars'),
+  body('date_of_birth', 'invalid date of birth')
+    .optional({ checkFalsy: true })
+    .isISO8601(),
+  body('date_of_death', 'invalid date of death')
+    .optional({ checkFalsy: true })
+    .isISO8601(),
+
+  //sanitize
+  sanitizeBody('first_name').escape(),
+  sanitizeBody('family_name').escape(),
+  sanitizeBody('date_of_birth').toDate(),
+  sanitizeBody('date_of_death').toDate(),
+
+  //process the req
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render('author_form', {
+        title: 'crete author (again)',
+        errors: errors.array(),
+        author: req.body,
+      });
+      return;
+    } else {
+      let author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+      });
+      author.save(function (err){
+        if (err){return next(err)}
+        res.redirect(author.url)
+      })
+    }
+  },
+];
 
 exports.author_delete_get = function (req, res) {
   res.send('NOT IMPLEMENTED: author delete get');
